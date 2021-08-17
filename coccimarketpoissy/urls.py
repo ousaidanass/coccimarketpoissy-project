a307fc5c69d01ea1b2dd0fbc40d3f5264dc07f25
ousaidanass <anassousaidanass@gmail.com>
2021-08-17 19:30:09 +0200

    urls.py static serving added

--- coccimarketpoissy/urls.py
+++ coccimarketpoissy/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
     path('produit/', include('gestionstock.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
